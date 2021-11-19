3b04b0b608e773ef2ef150ca2ff944fa00012714
Aleksa <aleksazdravkovicskola@gmail.com>
2021-11-19 17:58:32 +0100

    osnove

--- igrica.py
+++ igrica.py
@@ -1,8 +1,8 @@
 import pygame as pg
 
-pygame.init()
+pg.init()
 
-monitor = [pygame.display.Info().current_w, pygame.display.Info().current_h]
-sirina = monitor[0] * 50%
-visina = monitor[1] * 50%
-prozor = pygame.display.set_mode((sirina, visina))
+monitor = [pg.display.Info().current_w, pg.display.Info().current_h]
+sirina = monitor[0] / 2
+visina = monitor[1] / 2
+prozor = pg.display.set_mode((sirina, visina), pg.RESIZABLE)
