3c124387c06316f6ea71fdb4653d1de17b72a327
Aleksa <aleksazdravkovicskola@gmail.com>
2021-11-20 13:30:23 +0100

    osnove

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ monitor = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 sirina = monitor[0]
 visina = monitor[1]
 
-prozor = pygame.display.set_mode((sirina, visina))
+prozor = pygame.display.set_mode((sirina, visina), pygame.FULLSCREEN)
 pygame.display.set_caption("Test")
 sat = pygame.time.Clock()
 
-while True:
-    for dogadjaj in pygame.event.get():
-        if dogadjaj.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+def gameloop():
+    while True:
+        for dogadjaj in pygame.event.get():
+            if dogadjaj.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
-    pygame.display.update()
-    sat.tick(60)
+        pygame.display.update()
+        sat.tick(60)
+
+gameloop()
