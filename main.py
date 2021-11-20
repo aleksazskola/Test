45dabe716b47ae995cfbeffa31152453b11f7906
Aleksa <aleksazdravkovicskola@gmail.com>
2021-11-20 11:23:43 +0100

    osnove

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ sirina = monitor[0] / 2
 visina = monitor[1] / 2
 prozor = pygame.display.set_mode((sirina, visina))
 
+pygame.display.set_caption("Test")
+
 while True:
     for dogadjaj in pygame.event.get():
         if dogadjaj.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
     pygame.display.update()
