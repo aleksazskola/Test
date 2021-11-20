c4ba5659ec9eea6d14176e382f72eb4d36a1c624
Aleksa <aleksazdravkovicskola@gmail.com>
2021-11-20 11:20:23 +0100

    osnovna struktura

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
 monitor = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 sirina = monitor[0] / 2
 visina = monitor[1] / 2
-prozor = pygame.display.set_mode((sirina, visina), pygame.RESIZABLE)
+prozor = pygame.display.set_mode((sirina, visina))
+
+while True:
+    for dogadjaj in pygame.event.get():
+        if dogadjaj.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
